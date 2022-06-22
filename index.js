b1d78af58ee12d09e5a9e596fd143cead879bf88
unknown <akumareign@gmail.com>
2022-06-22 01:15:14 -0400

    counter button

---
+++ index.js
@@ -0,0 +1,11 @@
+let count = 0
+
+let clickerCount = document.getElementById("clicker-Count")
+
+console.log(counter)
+
+function increment () {
+    count += 1
+    clickerCount.innerText = count
+    console.log(count)
+}
